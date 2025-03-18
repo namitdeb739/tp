42e1444c5da4bb41275eb67484fd8fdf76628489
qi an <ongqian2002@gmail.com>
2025-03-18 22:57:54 +0800

    Refactor personlistpanel to tenantlistpanel
    Modify delete command messages
    
    Modify add command usage message

--- src/main/java/seedu/address/ui/TenantListPanel.java
+++ src/main/java/seedu/address/ui/TenantListPanel.java
@@ -10,36 +10,36 @@ import seedu.address.model.tenant.Tenant;
 /**
  * Panel containing the list of persons.
  */
-public class PersonListPanel extends UiPart<Region> {
-    private static final String FXML = "PersonListPanel.fxml";
+public class TenantListPanel extends UiPart<Region> {
+    private static final String FXML = "TenantListPanel.fxml";
     // private final Logger logger = LogsCenter.getLogger(PersonListPanel.class);
 
     @FXML
     private ListView<Tenant> personListView;
 
     /**
-     * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
+     * Creates a {@code TenantListPanel} with the given {@code ObservableList}.
      */
-    public PersonListPanel(ObservableList<Tenant> personList) {
+    public TenantListPanel(ObservableList<Tenant> personList) {
         super(FXML);
         personListView.setItems(personList);
-        personListView.setCellFactory(listView -> new PersonListViewCell());
+        personListView.setCellFactory(listView -> new TenantListViewCell());
     }
 
     /**
      * Custom {@code ListCell} that displays the graphics of a {@code Person} using a
      * {@code PersonCard}.
      */
-    class PersonListViewCell extends ListCell<Tenant> {
+    class TenantListViewCell extends ListCell<Tenant> {
         @Override
-        protected void updateItem(Tenant person, boolean empty) {
-            super.updateItem(person, empty);
+        protected void updateItem(Tenant tenant, boolean empty) {
+            super.updateItem(tenant, empty);
 
-            if (empty || person == null) {
+            if (empty || tenant == null) {
                 setGraphic(null);
                 setText(null);
             } else {
-                setGraphic(new TenantCard(person, getIndex() + 1).getRoot());
+                setGraphic(new TenantCard(tenant, getIndex() + 1).getRoot());
             }
         }
     }
