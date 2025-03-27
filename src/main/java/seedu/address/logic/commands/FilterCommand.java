ee9b0e80a9194d277f57dd81534d786750f6683e
leeyenshen <leeyenshen@gmail.com>
2025-03-27 12:38:06 +0800

    Fix checkstyle error

--- src/main/java/seedu/address/logic/commands/FilterCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -9,16 +9,16 @@ import seedu.address.model.tenant.AddressContainsKeywordsPredicate;
 
 /**
  * Filters for all persons in address book whose address contains any of the argument keywords.
- * Keyword matching is case insensitive.
+ * Keyword matching is case-insensitive.
  */
 public class FilterCommand extends Command {
     public static final String COMMAND_WORD = "filter";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Filters all persons whose addresses contain any of "
-            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n" + "Example: " + COMMAND_WORD
-            + " Kent Ridge";
+        + ": Filters all persons whose addresses contain any of "
+        + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+        + "Parameters: KEYWORD [MORE_KEYWORDS]...\n" + "Example: " + COMMAND_WORD
+        + " Kent Ridge";
 
     private final AddressContainsKeywordsPredicate predicate;
 
@@ -31,7 +31,7 @@ public class FilterCommand extends Command {
         requireNonNull(model);
         model.updateFilteredTenantList(predicate);
         return new CommandResult(String.format(Messages.MESSAGE_TENANTS_LISTED_OVERVIEW,
-                model.getFilteredTenantList().size()));
+            model.getFilteredTenantList().size()));
     }
 
     @Override
