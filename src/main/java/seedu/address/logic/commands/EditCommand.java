dae8f667dcc6b0ffb990e591854ffca4b991a28a
leeyenshen <leeyenshen@gmail.com>
2025-04-05 17:13:45 +0800

    Fixes error message for edit command

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -45,8 +45,8 @@ public class EditCommand extends Command {
             + PREFIX_PHONE + "91234567 " + PREFIX_EMAIL + "johndoe@example.com";
 
     public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person: %1$s";
-    public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided. eg. givenN/name |"
-        + " familyN/ surname | tag/ | address/ | phone| ";
+    public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided. eg. givenN/name"
+        + " + familyN/ surname | tag/ | address/ | phone| (giveN/ must be used with familyN)";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book.";
 
     private final Index index;
